ea4def35d90203602efb11597f5c938d16af74cf
Hanuman <hanumanj1995@gmail.com>
2022-09-18 22:46:31 +0530

    hh
    hh

--- Deomo/src/TestDemo.java
+++ Deomo/src/TestDemo.java
@@ -1,6 +1,10 @@
 
 public class TestDemo {
 public static void main(String[] args) {
+<<<<<<< Updated upstream
 	System.out.println("Hello Mdoo");
+=======
+	System.out.println("Hello MV");
+>>>>>>> Stashed changes
 }
 }
